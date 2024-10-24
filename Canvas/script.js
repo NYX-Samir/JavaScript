4b1409ec50258568427c34dfb6f9a00f782c2250
Syntax Error <157401490+Samir3vil@users.noreply.github.com>
2024-10-24 14:26:52 +0530

    Add files via upload

---
+++ Canvas/script.js
@@ -0,0 +1,15 @@
+
+const Canvas = document.getElementById("mycanvas");
+const brush = Canvas.getContext("2d");
+
+// brush.beginPath();
+// brush.moveTo(0,0);
+// brush.lineTo(250,250);
+// brush.lineTo(250,500);
+// brush.moveTo(250,250);
+// brush.lineTo(500,0);
+// brush.stroke();
+
+brush.fillText("I m Canvas",250,250);
+brush.fillstyle="red"
+brush.font = "30px cursive";
